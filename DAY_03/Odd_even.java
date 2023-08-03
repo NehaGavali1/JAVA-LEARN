c89b1586cbe1503d426c5976abbe4b59c94d0ce2
NehaGavali1 <88278891+NehaGavali1@users.noreply.github.com>
2023-08-03 10:26:32 +0000

    done

--- DAY_03/Odd_even.java
+++ DAY_03/Odd_even.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 public class Odd_even{   
  public static void main(String[]args){
  Scanner sc = new Scanner(System.in);
- int number;
  System.out.println("enter your following detect odd or even");
  System.out.println("enter your number");
- number = sc.nextInt();
+ int number = sc.nextInt();
         if(number%2==0){
             System.out.println("number is even");
         }
