0459c91eb9cc12693dab69cf86fce8451bc69065
NehaGavali1 <88278891+NehaGavali1@users.noreply.github.com>
2023-08-05 12:47:59 +0000

    done

--- Assignment.java
+++ Assignment.java
@@ -1,18 +1,30 @@
 
 import java.util.Scanner;
-public class Assignment{
+ class Assignment{
     public static void main(String[] args){
-        System.out.println("enter ther passwd");
-        System.out.println("renter the password");
+        
         Scanner sc=new Scanner(System.in);
+        System.out.println("enter ther passwd");
         int passwd=sc.nextInt();
+        System.out.println("renter the password");
         int passwd2=sc.nextInt();
-        if(passwd==passwd2)
+        
+        if(passwd == passwd2){
         System.out.println("passwd is match");
-        else
-        System.out.println("renter the passwd");
     }
-
-    }
-
+        else{
+        System.out.println("renter not match");
+        int attempt=3;
+        System.out.println("try again");
+        while(attempt>0)
+        {
+         System.out.println("try again");   
+        }
+        attempt--;
+        
+        
+        
 }
+}}
+
+
